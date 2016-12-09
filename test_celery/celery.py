fbae16a2b03dca0775cba24363876d39e6ab9ba9
Peter Sujan <peterasujan@gmail.com>
2016-12-09 11:43:12 -0800

    Update celery.py

--- test_celery/celery.py
+++ test_celery/celery.py
@@ -2,6 +2,6 @@ from __future__ import absolute_import
 from celery import Celery
 
 app = Celery('test_celery',
-             broker='redis://localhost:6379/0',
+             broker=''amqp://guest@localhost//',
              backend='rpc://',
              include=['test_celery.tasks'])
