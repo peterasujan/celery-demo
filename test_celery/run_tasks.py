42584f1e42db33279a4cbda222ea241b3a6a7fb2
Peter Sujan <peterasujan@gmail.com>
2016-12-09 16:23:53 -0800

    Update run_tasks.py

--- test_celery/run_tasks.py
+++ test_celery/run_tasks.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     print('Task finished? ', result.ready())
     print('Task result: ', result.result)
     # sleep 10 seconds to ensure the task has been finished
-    time.sleep(10)
+    time.sleep(1)
     # now the task should be finished and ready method will return True
     print('Task finished? ', result.ready())
     print('Task result: ', result.result)
