007eec938490171b16c8b05dd82aa6c5bc586c2c
Peter Sujan <pesujan@microsoft.com>
2016-12-14 13:34:48 -0800

    Use env var for broker.

--- test_celery/celery.py
+++ test_celery/celery.py
@@ -1,7 +1,13 @@
 from __future__ import absolute_import
+import os
 from celery import Celery
 
+_RABBIT = 'localhost'
+if 'RABBIT_SERVER' in os.environ:
+    _RABBIT = os.environ['RABBIT_SERVER']
+_BROKER = 'amqp://guest:guest@{}//'.format(_RABBIT)
+
 app = Celery('test_celery',
-             broker='amqp://guest:guest@some-rabbit//',
+             broker=_BROKER,
              backend='rpc://',
              include=['test_celery.tasks'])
